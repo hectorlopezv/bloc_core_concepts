d393fdc09eb889cf90c8f941c87c095f916ed0c8
hvlopez <hectorvmlopez@gmail.com>
2022-07-07 14:21:48 -0500

    named routes navigation

--- bloccoreconcepts/lib/bloc_core_concepts/bloc_core_concepts_cubit_main.dart
+++ bloccoreconcepts/lib/bloc_core_concepts/bloc_core_concepts_cubit_main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
+    final cubit = CounterCubit();
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
