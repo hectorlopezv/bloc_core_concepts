e286ac02a39b585be773aadebf670670023783ab
hvlopez <hectorvmlopez@gmail.com>
2022-07-07 14:27:40 -0500

    onGenerate Route Navigation

--- bloccoreconcepts/lib/bloc_access_navigation/main.dart
+++ bloccoreconcepts/lib/bloc_access_navigation/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloccoreconcepts/bloc_access_navigation/ui/screens/home_screen.dart';
 import 'package:bloccoreconcepts/bloc_access_navigation/logic/cubit/counter_cubit.dart';
+import 'package:bloccoreconcepts/bloc_access_navigation/ui/screens/router/app_router.dart';
 import 'package:bloccoreconcepts/bloc_access_navigation/ui/screens/second_screen.dart';
 import 'package:bloccoreconcepts/bloc_access_navigation/ui/screens/third_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,40 +16,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final CounterCubit _counterCubit = CounterCubit();
+  final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: HomeScreen(
-                title: 'Home Screen',
-                colors: Colors.blue,
-              ),
-            ),
-        '/second': (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: SecondScreen(
-                title: 'Second',
-                colors: Colors.red,
-              ),
-            ),
-        "/third": (context) => BlocProvider.value(
-              value: _counterCubit,
-              child: ThirdScreen(
-                title: 'Third',
-                colors: Colors.green,
-              ),
-            ),
-      },
+      onGenerateRoute: _appRouter.onGenerateRoute,
     );
   }
 
   @override
   void dispose() {
-    _counterCubit.close();
+    _appRouter.dispose();
     super.dispose();
   }
 }
